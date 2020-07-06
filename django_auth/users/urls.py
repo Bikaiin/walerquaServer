7089632480ac813ef5e65586468cb7ac5175b42d
Евгений Грингаут <evgenijgringaut@MacBook-Pro.local>
2020-07-06 22:19:30 +0300

    first commit

---
+++ django_auth/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import CreateUserAPIView, UserRetrieveUpdateAPIView, authenticate_user
+ 
+urlpatterns = [
+    path('create', CreateUserAPIView.as_view()),
+    path('obtain_token', authenticate_user),
+    path('update', UserRetrieveUpdateAPIView.as_view())
+   
+]
